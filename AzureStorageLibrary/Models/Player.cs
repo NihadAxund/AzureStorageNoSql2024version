dcd1755d14ec5f6e5c6e758d4e863f674cd39e20
NihadAxund <coinnihad@gmail.com>
2024-05-02 19:35:22 +0400

    Add project files.

---
+++ AzureStorageLibrary/Models/Player.cs
@@ -0,0 +1,38 @@
+﻿
+using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageLibrary.Models
+{
+    public class Player:ITableEntity
+    {
+        public Player() {
+            
+        }
+        public Player(string? name, string? surname, string? birthDate, double salary, double score, string partitionKey)
+        {
+            Name = name;
+            Surname = surname;
+            BirthDate = birthDate;
+            Salary = salary;
+            Score = score;
+            PartitionKey = partitionKey;
+            RowKey = Guid.NewGuid().ToString();
+        }
+
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? BirthDate { get; set; }
+        public double Salary { get; set; }
+        public double Score { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; } = DateTimeOffset.MinValue;
+        public ETag ETag { get; set; } = ETag.All;
+    }
+}
---
+++ MVCWebApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using AzureStorageLibrary.Models;
+using AzureStorageLibrary.Services;
+using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.Models;
+using System.Diagnostics;
+
+namespace MVCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly INoSqlStorage<Player> _noSqlStorage;
+
+        public HomeController(INoSqlStorage<Player> noSqlStorage)
+        {
+            _noSqlStorage = noSqlStorage;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var player = new Player("player", "playerov", new DateOnly(2005, 05, 05).ToString(), 1000, 12, "Players");
+            await _noSqlStorage.Add(player);
+            ViewBag.players = (await _noSqlStorage.All()).ToList();
+            return View();
+        }
+    }
+}
